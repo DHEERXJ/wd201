3f68cea03719e742229f27f127d323acfe92a6fa
Dheeraj Krishna <70833570+DHEERXJ@users.noreply.github.com>
2023-04-06 00:34:31 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,12 +1,10 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
+const db = {};
+const hi = {};
+const datee = new Date().toLocaleDateString("en-CA");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
@@ -15,15 +13,51 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
+    static getTodos() {
+      return this.findAll();
+    }
+
+    static overdueTodos() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: datee,
+          },
+        },
+      });
+    }
+
+    static todayTodos() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: datee,
+          },
+        },
+      });
+    }
+
+    static laterTodos() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: datee,
+          },
+        },
+      });
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
     }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
-      dueDate: DataTypes.DATEONLY,
       completed: DataTypes.BOOLEAN,
+      dueDate: DataTypes.DATEONLY,
+      
     },
     {
       sequelize,
